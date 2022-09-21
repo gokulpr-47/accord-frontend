2999dfed74833ac7146b07779e84de9e8d1385f8
gokulpr-47 <gokulpr47@gmail.com>
2022-09-21 20:32:42 +0530

    changed socket connection

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { AuthProvider } from './Context/AuthProvider'
 import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'
 import { ChatProvider } from './Context/ChatContext'
 import io from 'socket.io-client'
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://acccord.herokuapp.com/")
 
 export default function App(){
 
