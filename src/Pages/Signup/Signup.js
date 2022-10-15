2a0d0883f4e57d1aa66965173d802abab38edc16
gokulpr-47 <gokulpr47@gmail.com>
2022-10-15 17:28:05 +0530

    changed import for axios

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from '../../api/axios'
 import {useState, useContext, useRef, useEffect} from 'react'
 // import {Container} from 'react-bootstrap'
 import "./signup.css"
