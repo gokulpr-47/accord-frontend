8cc2eb5f92df38b286be61705d252db811a2c483
gokulpr-47 <gokulpr47@gmail.com>
2023-03-04 13:40:46 +0530

    changed backend url

--- src/App.js
+++ src/App.js
@@ -1,44 +1,63 @@
-import { useState } from 'react'
-import Signin from './Pages/Signin/Signin'
-import Signup from './Pages/Signup/Signup'
-import Chat from './Pages/Chat/Chat'
-import PersistLogin from './Pages/PersistLogin'
-import UserContext from './Context/UserContext'
-import { AuthProvider } from './Context/AuthProvider'
-import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'
-import { ChatProvider } from './Context/ChatContext'
-import io from 'socket.io-client'
-// const socket = io("https://acccord.herokuapp.com", {
-const socket = io("http://localhost:3000", {
-    withCredentials: true,
-    transports: ["websocket", "polling"] // use WebSocket first, if available
+import { useState } from "react";
+import Signin from "./Pages/Signin/Signin";
+import Signup from "./Pages/Signup/Signup";
+import Chat from "./Pages/Chat/Chat";
+import PersistLogin from "./Pages/PersistLogin";
+import UserContext from "./Context/UserContext";
+import { AuthProvider } from "./Context/AuthProvider";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { ChatProvider } from "./Context/ChatContext";
+import io from "socket.io-client";
+const socket = io("https://acccord-backend.onrender.com", {
+  // const socket = io("http://localhost:3000", {
+  withCredentials: true,
+  transports: ["websocket", "polling"], // use WebSocket first, if available
 });
 
-export default function App(){
+export default function App() {
+  const [user, setUser] = useState(localStorage.getItem("username") || "");
+  const [authEmail, setAuthEmail] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const [ user, setUser ] = useState(localStorage.getItem('username') || '')
-    const [ authEmail, setAuthEmail ] = useState() 
-    const [ isLoggedIn, setIsLoggedIn ] = useState(false)
+  return (
+    <Router>
+      <AuthProvider>
+        <ChatProvider>
+          <UserContext.Provider
+            value={{
+              user,
+              setUser,
+              authEmail,
+              setAuthEmail,
+              isLoggedIn,
+              setIsLoggedIn,
+            }}
+          >
+            <Routes>
+              <Route path="*" element={<Navigate to="/channels" replace />} />
+              <Route path="/Signin" element={<Signin />} />
+              <Route path="/signup" element={<Signup />} />
 
-    return(
-       <Router>
-            <AuthProvider>
-                <ChatProvider>
-                    <UserContext.Provider value={{ user, setUser, authEmail, setAuthEmail, isLoggedIn, setIsLoggedIn }}>
-                        <Routes>
-                            <Route path='*' element={<Navigate to="/channels" replace/>} />
-                            <Route path='/Signin' element={<Signin/>} />
-                            <Route path='/signup' element={<Signup/>} />
-
-                            <Route element={<PersistLogin/>} >
-                                <Route path='channels' element={<Chat socket={socket}/>} />
-                                <Route path='channels/:serverId' element={<Chat socket={socket}/>} />
-                                <Route path='channels/:serverId/:channelId' element={<Chat socket={socket}/>} />
-                            </Route>
-                        </Routes>
-                    </UserContext.Provider>
-                </ChatProvider>
-            </AuthProvider>
-       </Router>
-    )
+              <Route element={<PersistLogin />}>
+                <Route path="channels" element={<Chat socket={socket} />} />
+                <Route
+                  path="channels/:serverId"
+                  element={<Chat socket={socket} />}
+                />
+                <Route
+                  path="channels/:serverId/:channelId"
+                  element={<Chat socket={socket} />}
+                />
+              </Route>
+            </Routes>
+          </UserContext.Provider>
+        </ChatProvider>
+      </AuthProvider>
+    </Router>
+  );
 }
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
-// const BASE_URL = 'https://acccord.herokuapp.com';
-const BASE_URL = 'http://localhost:3001';
+import axios from "axios";
+const BASE_URL = "https://acccord-backend.onrender.com";
+// const BASE_URL = 'http://localhost:3001';
 export default axios.create({
-    baseURL: BASE_URL
+  baseURL: BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
